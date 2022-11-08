152896fb9a59c076ac29d73b58d5e2030a325af2
shovonkhan200h <shovonkhan00@gmail.com>
2022-11-09 05:35:30 +0600

    map filter find

---
+++ mapFilerFind.js
@@ -0,0 +1,27 @@
+let number = [100,200,300,400]
+const output = []
+for (let i = 0; i < number.length; i++) {
+    let element = number[i];
+    element = Math.round(Math.random()*10)
+    output.push(element)
+}
+
+console.log(output)
+
+const result = number.map(number => number * number)
+
+const output = number.map((num)=>{
+    return num + num;
+})
+console.log(output)
+
+
+// MAP ONLY WORKS WITH ARRAY
+
+
+const output2= number.filter(x => x >200)
+console.log(output2)
+
+
+const output3= number.find(name => name > 100 )
+console.log(output3)
