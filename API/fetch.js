139c0b0d6214cbdb458a2738df8e3a3ee1c122c9
shovonkhan200h <shovonkhan00@gmail.com>
2022-11-14 01:12:22 +0600

    new update

--- API/fetch.js
+++ API/fetch.js
@@ -1,18 +1,20 @@
-// fetch('https://jsonplaceholder.typicode.com/users')
-// .then(response => response.json())
-// .then(json => dataBase(json))
-// .catch(erro => console.log(erro));
+fetch('https://jsonplaceholder.typicode.com/users')
+.then(response => response.json())
+.then(json => dataBase(json))
+.catch(erro => console.log(erro));
 
-// const dataBase = (users)=>{
-//     const displayName = document.getElementById(`displayValue`)
-//     const userName = users.map(user => user.username)
-//     for(let i=0; i<userName.length; i++){
-//         const element = userName[i]
-//         const li = document.createElement(`li`)
-//         li.innerText = element;
-//         displayName.appendChild(li)
-//     }
-// }
+const dataBase = (users)=>{
+    const displayName = document.getElementById(`displayValue`)
+    const userName = users.map(user => user.username)
+    for(let i=0; i<userName.length; i++){
+        const element = userName[i]
+        const li = document.createElement(`li`)
+        li.innerText = element;
+        displayName.appendChild(li)
+    }
+}
+
+
 document.getElementById(`submit`).addEventListener(`click`, ()=>{
     const userContent = document.getElementById(`user-container`).value;
     const bodyContent = document.getElementById(`body-content`).value;
@@ -44,5 +46,31 @@ const postToserver =(userInfo)=>{
         }
     }
 }
+
+// const userInfo={
+//     title: 'foo',
+//     body: 'bar',
+//     userId: 1,
+//     }
+// const submitButton = document.getElementById(`submit`).addEventListener(`click`,()=>{
+//     const userContent = document.getElementById(`user-container`).value;
+//     const userBody = document.getElementById(`body-content`).value;
+//     const post = {id:userContent, body:userBody}
+//     postToserver(post)
+// })
+
+// const postToserver =(userInfo)=>{
+//     fetch('https://jsonplaceholder.typicode.com/posts', {
+//     method: 'POST',
+//     body: JSON.stringify(userInfo),
+//     headers: {
+//     'Content-type': 'application/json; charset=UTF-8',
+//     },
+// })
+//     .then((response) => response.json())
+//     .then((json) => console.log(json));
+// }
+
+
 
 
