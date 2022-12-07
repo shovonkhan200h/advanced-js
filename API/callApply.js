35f4c8ca2e61004eaee78549539005a3a83a6361
shovonkhan200h <shovonkhan00@gmail.com>
2022-12-08 03:35:50 +0600

    new update on callApply.js

--- API/callApply.js
+++ API/callApply.js
@@ -1,9 +1,16 @@
 const nameAs={
     firstName:`shovon`,
     lastName:`khan`,
+    salar:6000,
     getFullName:function(){
         console.log(this.firstName, this.lastName);
+    },
+    salaryCount:function(amount){
+        this.salar = this.salar - amount;
+        return this.salar;
     }
+
 }
 
-console.log(nameAs)
+nameAs.salaryCount(1000);
+console.log(nameAs.salar)
